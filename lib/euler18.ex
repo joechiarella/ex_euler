9152e8de12fb5fb61fec58206f106458bc59b5ac
Joe Chiarella <joe.chiarella@salesloft.com>
2018-10-15 15:02:35 -0400

    Solved 18 (and thus 67) efficiently

---
+++ lib/euler18.ex
@@ -0,0 +1,74 @@
+defmodule Euler18 do
+  @moduledoc """
+  By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+  3
+  7 4
+  2 4 6
+  8 5 9 3
+
+  That is, 3 + 7 + 4 + 9 = 23.
+
+  Find the maximum total from top to bottom of the triangle below:
+
+  75
+  95 64
+  17 47 82
+  18 35 87 10
+  20 04 82 47 65
+  19 01 23 75 03 34
+  88 02 77 73 07 63 67
+  99 65 04 28 06 16 70 92
+  41 41 26 56 83 40 80 70 33
+  41 48 72 33 47 32 37 16 94 29
+  53 71 44 65 25 43 91 52 97 51 14
+  70 11 33 28 77 73 17 78 39 68 17 57
+  91 71 52 38 17 14 91 43 58 50 27 29 48
+  63 66 04 68 89 53 67 30 73 16 69 87 40 31
+  04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+  """
+
+  def solve(triangle) do
+    string_to_lists(triangle)
+    |> reduce
+  end
+
+  def reduce([[last_value] | []]) do
+    last_value
+  end
+
+  def reduce([bottom_row | [second_row | rest]]) do
+    merged_row = reduce_row(bottom_row)
+    |> merge_with(second_row)
+
+    reduce([merged_row | rest])
+  end
+
+  def merge_with(first, second) do
+    Enum.zip(first, second)
+    |> Enum.map(fn {a, b} -> a + b end)
+  end
+
+  def reduce_row([a | []]) do
+    [ a ]
+  end
+
+  def reduce_row([a | [b | []]]) do
+    [ max(a, b) ]
+  end
+
+  def reduce_row([a | [b | rest]]) do
+    [ max(a, b) | reduce_row([b | rest]) ]
+  end
+
+  def string_to_lists(triangle) do
+    String.split(triangle, "\n", trim: true)
+    |> Enum.map(&row_to_list(&1))
+    |> Enum.reverse()
+  end
+
+  def row_to_list(row) do
+    String.split(row, " ")
+    |> Enum.map(&String.to_integer(&1))
+  end
+end
