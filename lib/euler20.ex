4a7f980ce137b9e287ac3b38713b7bd7cf8c66b1
Joe Chiarella <joe.chiarella@salesloft.com>
2018-10-16 08:41:48 -0400

    Solution for #20

---
+++ lib/euler20.ex
@@ -0,0 +1,18 @@
+defmodule Euler20 do
+  @moduledoc """
+  Find the sum of the digits in the number 100!
+  """
+
+  def solve() do
+    factorial(100)
+    |> Integer.to_charlist
+    |> Enum.map(&(&1 - ?0))
+    |> Enum.sum
+  end
+
+  def factorial(1), do: 1
+  def factorial(n) do
+    n * factorial(n - 1)
+  end
+
+end
---
+++ test/euler20_test.exs
@@ -0,0 +1,8 @@
+defmodule Euler20Test do
+  use ExUnit.Case
+
+  test "Solve Euler #20" do
+    assert Euler20.solve() == 648
+  end
+
+end
