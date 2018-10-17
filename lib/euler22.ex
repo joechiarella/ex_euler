d823c063c2c80ee29e55a30d6ed2107643edd36f
Joe Chiarella <joe.chiarella@salesloft.com>
2018-10-17 14:44:47 -0400

    Solution for #22

---
+++ lib/euler22.ex
@@ -0,0 +1,35 @@
+defmodule Euler22 do
+  @moduledoc """
+  Using names.txt (right click and 'Save Link/Target As...'), a 46K text file
+  containing over five-thousand first names, begin by sorting it into
+  alphabetical order. Then working out the alphabetical value for each name,
+  multiply this value by its alphabetical position in the list to obtain a
+  name score.
+
+  For example, when the list is sorted into alphabetical order, COLIN, which
+  is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So,
+  COLIN would obtain a score of 938 × 53 = 49714.
+
+  What is the total of all the name scores in the file?
+  """
+
+  def solve() do
+    File.read!("data/p022_names.txt")
+    |> String.split(",")
+    |> Enum.map(&(String.trim(&1, "\"")))
+    |> Enum.sort
+    |> Enum.with_index(1)
+    |> Enum.map(&name_score/1)
+    |> Enum.sum
+  end
+
+  def name_score({name, index}) do
+    value = String.to_charlist(name)
+    |> Enum.map(&(&1 - ?A + 1))
+    |> Enum.sum
+    value * index
+  end
+
+
+
+end
---
+++ test/euler22_test.exs
@@ -0,0 +1,8 @@
+defmodule Euler22Test do
+  use ExUnit.Case
+
+  test "Solve #22" do
+    assert Euler22.solve() == 0
+  end
+
+end
